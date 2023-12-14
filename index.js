d2ab91c140227d1ad896e0f863fdd2f2f7068da9
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-12-13 23:01:10 -0300

    progredindo...

---
+++ index.js
@@ -0,0 +1,23 @@
+// Configuração inicial
+const express = require('express') // Importa o arquivo express
+const app = express()  // starta o arquivo express
+
+// Forma de ler JSON - Middlewares -> Recursos que são executados entre nossas requisições e respostas
+app.use(
+    express.urlencoded({  // Lê JSON essa configuração
+        extended: true,
+    }),
+)
+
+app.use(express.json()) // Responde JSON esaa configuração
+
+// Rota inicial - endpoint
+app.get('/', (req, res) => {  // a '/' = ponto de acesso, (req, res) é um padrão, onde você recebe uma requisição e pode responder se desejar
+    
+    res.json({Felipe: 'Estou aprendendo criar uma API Node =)'}) //  Mandando uma res no formato json, OBS: no lugar de message poderia ser qualquer palavra ex: msg, adendo, aviso etc...
+    
+
+})
+
+// Entregar uma porta
+app.listen(3000) // Concretizando a disponibilidade da porta 3000
